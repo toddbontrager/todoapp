8896188444cb981905413f94923204b68372bb74
Todd <bontragertodd@gmail.com>
2018-12-02 11:13:24 -0500

    Rename controller. Add GET method for returning one todo

---
+++ Controllers/ToDosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers
+{
+    // route template
+    [Route("api/[controller]")]
+    public class ToDosController : Controller
+    {
+        // GET api/todos
+        [HttpGet]
+        public JsonResult GetTodos()
+        {
+            return new JsonResult(ToDoDataStore.Current.ToDos);
+        }
+
+        // GET api/todos/2
+        [HttpGet("{id}")]
+        public JsonResult GetToDo(int id)
+        {
+            return new JsonResult(ToDoDataStore.Current.ToDos.FirstOrDefault(t => t.Id == id));
+        }
+
+
+        // POST
+
+
+        // DELETE
+    }
+}
