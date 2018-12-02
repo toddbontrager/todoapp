25c258964295572e6ea060ffb08275c3d2df2422
Todd <bontragertodd@gmail.com>
2018-12-02 09:19:10 -0500

    initial commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    
+    public class ToDoController : Controller
+    {
+        // GET api/todos
+        [HttpGet]
+        public IActionResult GetTodos()
+            return Ok()
+
+        // GET api/todos/3
+    }
+}
---
+++ Models/ToDoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDoDto
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+    }
+}
