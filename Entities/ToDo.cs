7cdde3b78c64704c6c9798b7ac6a62cf2bd0c7cf
Todd <bontragertodd@gmail.com>
2018-12-05 11:44:48 -0500

    Add entity class

---
+++ Entities/ToDo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoApp.Entities
+{
+    public class ToDo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Task { get; set; }
+    }
+}
