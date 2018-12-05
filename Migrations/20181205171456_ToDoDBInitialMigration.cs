354bf00b9444b21de97c8ce90ab4bbbe09b8e5a5
Todd <bontragertodd@gmail.com>
2018-12-05 12:23:25 -0500

    add migrations

---
+++ Migrations/20181205171456_ToDoDBInitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoApp.Migrations
+{
+    public partial class ToDoDBInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Task = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
