569f020fe7d82cf8c14f1db1b7951c592673ba86
Todd <bontragertodd@gmail.com>
2018-12-05 15:00:14 -0500

    Add seed data

---
+++ ToDoContextExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Entities;
+
+namespace ToDoApp
+{
+    public static class ToDoContextExtensions
+    {
+        public static void EnsureSeedDataForContext(this ToDoContext context)
+        {
+            if (context.ToDos.Any())
+            {
+                return;
+            }
+
+            // init seed data
+            var todos = new List<ToDo>()
+            {
+                new ToDo()
+                {
+                    Task = "Wash car",
+                },
+                new ToDo()
+                {
+                    Task = "Rake leaves"
+                }
+            };
+
+            context.ToDos.AddRange(todos);
+            context.SaveChanges();
+        }
+    }
+}
