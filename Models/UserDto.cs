ff24315a245689a20df7d39ec7a90bf5c56b9bf3
Todd <bontragertodd@gmail.com>
2018-12-12 12:39:46 -0500

    Create models

---
+++ Models/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class UserDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
