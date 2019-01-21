41246ef9e2222abd550ddee37f81bbee1181fa7f
Todd <bontragertodd@gmail.com>
2019-01-21 14:37:40 -0500

    Create interface and services

---
+++ Services/Authentication.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using ToDoApp.Models;
+
+
+namespace ToDoApp.Services
+{
+    public class Authentication : IAuthentication
+    {
+        private readonly IConfiguration _config;
+
+        public Authentication(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public UserDto Authenticate(LoginDto login)
+        {
+            UserDto user = null;
+
+            if (login.Username == "todd" && login.Password == "secret")
+            {
+                user = new UserDto
+                {
+                    Name = "Todd",
+                    Email = "test@gmail.com"
+                };
+            }
+
+            return user;
+        }
+
+        public string BuildToken(UserDto user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Issuer"],
+             expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
