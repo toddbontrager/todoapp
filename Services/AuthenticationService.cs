315c0e18efe177e86ceb1b4f1ee9a0a66130547f
Todd <bontragertodd@gmail.com>
2019-01-21 14:55:12 -0500

    Add claims

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
@@ -40,10 +41,17 @@ namespace ToDoApp.Services
         {
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Name),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Birthdate, user.Birthdate.ToString("yyyy-MM-dd")),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
 
             var token = new JwtSecurityToken(_config["Jwt:Issuer"],
             _config["Jwt:Issuer"],
-             expires: DateTime.Now.AddMinutes(30),
+            claims,
+            expires: DateTime.Now.AddMinutes(30),
             signingCredentials: creds);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
