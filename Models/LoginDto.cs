da7acd9be1763ca0b0115eb1e5cc33c3eaff89ad
Todd <bontragertodd@gmail.com>
2019-01-21 14:25:12 -0500

    Create models

---
+++ Models/LoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
